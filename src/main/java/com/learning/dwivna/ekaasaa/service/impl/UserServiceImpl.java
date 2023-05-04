d59f06e3d9191bc66fe0f1e72c1ba8022a11ca54
Abhinav Dwivedi <a.dwivedispu@gmail.com>
2023-05-05 01:02:44 +0530

    Code Fix For Reactive Programming

--- src/main/java/com/learning/dwivna/ekaasaa/service/impl/UserServiceImpl.java
+++ src/main/java/com/learning/dwivna/ekaasaa/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.learning.dwivna.ekaasaa.service.impl;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.learning.dwivna.ekaasaa.data.User;
+import com.learning.dwivna.ekaasaa.service.PublisherService;
 import com.learning.dwivna.ekaasaa.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
@@ -12,6 +14,7 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -19,6 +22,7 @@ import reactor.core.publisher.Mono;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -37,57 +41,68 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private ChannelTopic channelTopic;
 
+    @Autowired
+    private PublisherService publisherService;
+
+    private final Gson gson = new GsonBuilder().create();
+
     @Override
     public Mono<User> getUser(String id) {
         return redisValueUserOperations.get(id);
     }
 
     @Override
+    public Mono<List<User>> getUsers() {
+        return reactiveUserRedisTemplate.keys("*").collectList().flatMap(keyList -> reactiveUserRedisTemplate.opsForValue().multiGet(keyList));
+    }
+
+    @Override
     public Mono<String> putUser(User user) {
         user.setId(UUID.randomUUID().toString());
-        Mono<Boolean> result = redisValueUserOperations.set(user.getId(), user);
-        return Boolean.TRUE.equals(result.block()) ? Mono.just(String.format("User added with ID:{%s}", user.getId())) : Mono.just("User addition failed");
+        return redisValueUserOperations.set(user.getId(), user).flatMap(res -> res ? Mono.just(String.format("User added with ID:{%s}", user.getId())) : Mono.just("User addition failed"));
     }
 
     @Override
     public Mono<User> updateUser(String id, User user) {
         user.setId(id);
-        redisValueUserOperations.getAndSet(id, user).block();
+        redisValueUserOperations.getAndSet(id, user).subscribe();
         return Mono.just(user);
     }
 
     @Override
     public Mono<String> deleteUser(String id) {
-        Mono<Boolean> result = redisValueUserOperations.delete(id);
-        return Boolean.TRUE.equals(result.block()) ? Mono.just(String.format("User deleted having ID:{%s}", id)) : Mono.just("User deletion failed");
+        return redisValueUserOperations.delete(id).flatMap(res -> res ? Mono.just(String.format("User deleted having ID:{%s}", id)) : Mono.just("User deletion failed"));
     }
 
     @Override
     public Publisher<List<User>> userSub() {
-        Flux<String> flux = reactiveMsgListenerContainer.receive(this.channelTopic)
+        return this.subscriptionResponse();
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void subscribeApp() {
+        List<User> userList = Objects.requireNonNull(this.getUsers().block());
+        log.info("Size of fetched keys is {}", userList.size());
+        publisherService.publish(gson.toJson(userList));
+    }
+
+    private Flux<String> getSubscriptionMessage(){
+        return reactiveMsgListenerContainer.receive(this.channelTopic)
                 .map(ReactiveSubscription.Message::getMessage)
                 .map(msg -> {
                     System.out.println(msg);
                     return msg;
                 });
-        log.info("userSub Message:{}", flux.blockFirst());
-        Gson gson = new Gson();
-        Type typeOfObjectsList = new TypeToken<List<User>>() {
-        }.getType();
-        List<User> users = gson.fromJson(flux.blockFirst(), typeOfObjectsList);
-        log.info("Message List:{}", users);
-        return Flux.just(users);
     }
 
-    @Override
-    public Mono<List<User>> getUsers() {
-        List<String> keys = reactiveUserRedisTemplate.keys("*").collectList().block();
-        List<User> userList = new ArrayList<>();
-        if (keys != null && !keys.isEmpty()) {
-            keys.forEach(key -> {
-                userList.add(this.getUser(key).block());
-            });
-        }
-        return Mono.just(userList);
+    private Flux<List<User>> subscriptionResponse() {
+        return this.getSubscriptionMessage().flatMap(subscribeData -> {
+            log.info("UserSub Message:{}", subscribeData);
+            Type typeOfObjectsList = new TypeToken<List<User>>() {
+            }.getType();
+            List<User> users = gson.fromJson(subscribeData, typeOfObjectsList);
+            log.info("Message List:{}", users);
+            return Mono.just(users);
+        });
     }
 }
